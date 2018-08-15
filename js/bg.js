faa316b8cf915abf3393438ae66c13262150f728
jfitzsimmons <joefitzsimmons@Corinnes-MacBook-Pro.local>
2018-08-15 10:11:58 -0500

    multiple circles, randomized / added css

--- js/bg.js
+++ js/bg.js
@@ -1,187 +1,78 @@
-/**
-const cvs = document.getElementById('bg');
-const ctx = cvs.getContext('2d');
-const box = 32;
-
-// Load images
-// add images | should audio be audio file?
-/**
-const groundImg = new Image();
-groundImg.src = 'img/ground.png';
-const foodImg = new Image();
-foodImg.src = 'img/food.png';
-*/
-
 var radius = 0;
-var color = 209;
-var colorend = color - 100;
-var light = 71;
-var lightend = light + 20;
+var strokeColors = ['#506EE5','#68B2F8','7037CD'];
+var colors = [209,291,263];
 
-function draw() {
-  var canvas = document.getElementById('bg');
-  if (canvas.getContext) {
-     var ctx = canvas.getContext('2d');
-     ctx.beginPath();
+var canvas = document.getElementById('bg');
+var ctx = canvas.getContext('2d');
+var circles = [];
+var requestAnimationFrame = window.requestAnimationFrame ||
+  window.mozRequestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  window.msRequestAnimationFrame;
+
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
+
+ctx.lineWidth = 1;
 
 
 
+var circumStroke = Math.PI * 2;
+var start = Math.PI / -2; // Start position (top)
+var finish = 100; // Finish (in %)
 
 
-  //  ctx.moveTo(110, 75);
-    //ctx.lineWidth=10;
-    //ctx.strokeStyle="#880000";
-    //ctx.stroke();
-    //ctx.fillStyle="#000088";
 
-    radius += 0.025;
-    if (radius < 2.5){
-      ctx.arc(75, 75, radius, 0, Math.PI * 2, false); // Outer circle
-      ctx.fillStyle = 'hsl(' + (colorend < color ? colorend+=1 : colorend = color) + ',100%,' + (light < lightend ? light+=0.2 : light = lightend) + '%)';
+function Circle() {
+  radius = 0;
+  var randomRad = Math.random() * (5 - 2.5) + 2.5;
+  var radiusStroke = randomRad + 3.5;
+  var x = Math.random() * (ctx.canvas.width);
+  var y = Math.random() * (ctx.canvas.height);
+  curr = 0;
+  var color = colors[Math.floor(Math.random()*colors.length)];
+  ctx.strokeStyle = strokeColors[Math.floor(Math.random()*strokeColors.length)];
+  var colorStart = Math.floor(Math.random() * (120 - 80) + 80);
+  var light = Math.floor(Math.random() * (60 - 40) + 40);
+  var lightend = light + Math.floor(Math.random() * (45 - 25) + 25);
+
+  function draw() {
+    ctx.beginPath();
+    radius += (randomRad / 100);
+    if (radius < randomRad) {
+      ctx.arc(x, y, radius, 0, Math.PI * 2, false); // inner circle
+      ctx.fillStyle = 'hsl(' + (colorStart < color ? colorStart += 1 : colorStart = color) + ',100%,' + (light < lightend ? light += 0.2 : light = lightend) + '%)';
       ctx.fill();
       requestAnimationFrame(draw);
-    }
-    ctx.closePath();
-    // CANVAS PROPERTIES
-
-  }
-}
-var canvas2 = document.getElementById('bg');
-var ctx2 = canvas2.getContext('2d');
-ctx2.lineWidth = 1;
-ctx2.strokeStyle = '#506EE5';
-
-
-// CANVAS MATHS
-var x = 75
-    y = 75,
-    radiusStroke = 6,
-    circumStroke = Math.PI * 2,
-    start = Math.PI / -2, // Start position (top)
-    finish = 100, // Finish (in %)
-    curr = 0; // Current position (in %)
-
-// CANVAS ANIMATION
-
-// Enables browser-decided smooth animation (60fps)
-var raf =
-    window.requestAnimationFrame ||
-    window.mozRequestAnimationFrame ||
-    window.webkitRequestAnimationFrame ||
-    window.msRequestAnimationFrame;
-window.requestAnimationFrame = raf;
-
-// Animate function
-function animate(draw_to) {
-  // Clear off the canvas
-  ctx2.clearRect(300, 100, 20, 20);
-  // Start over
-  ctx2.beginPath();
-  // arc(x, y, radius, startAngle, endAngle, anticlockwise)
-  // Re-draw from the very beginning each time so there isn't tiny line spaces between each section (the browser paint rendering will probably be smoother too)
-  ctx2.arc(x, y, radiusStroke, start, draw_to, false);
-  // Draw
-  ctx2.stroke();
-  // Increment percent
-  curr++;
-  // Animate until end
-  if (curr < finish + 1) {
-    // Recursive repeat this function until the end is reached
-    requestAnimationFrame(function () {
-      animate(circumStroke * curr / 100 + start);
-    });
-  }
-  ctx2.closePath();
-}
-
-animate();
-
-
-
-
-
-// Animation adpated from: http://stackoverflow.com/questions/15692353/animate-a-canvas-circle-to-draw-on-load & http://jsfiddle.net/loktar/uhVj6/4/
-
-
-
-
-
-
-
-
-
-/**
-
-
-
-// creat snake
-let snake = [];
-snake[0] = {
-  x: 9 * box,
-  y: 10 * box,
-};
-
-// create food
-let food = {
-  x: Math.floor(Math.random() * 17 + 1) * box,
-  y: Math.floor(Math.random() * 15 + 3) * box,
-};
-
-
-/**
- * snake collides with itself or border
- * @param {index} head index / head of snake.
- * @param {array} array entire snake.
- * @return {boolean} true or false.
- */
- /**
-function collision(head, array) {
-  for (let i = 0; i < array.length; i++) {
-    if (head.x == array[i].x && head.y == array[i].y) {
-      return true;
+    } else {
+      newCircle();
     }
   }
-  return false;
-}
-// check for border collisions too
 
-
-/**
- * draws canvas
- */
- /**
-function draw() {
-
-  for (let i = 0; i < snake.length; i++) {
-    ctx.fillStyle = (i == 0) ? 'green' : 'white';
-    ctx.fillRect(snake[i].x, snake[i].y, box, box);
-    ctx.strokeStyle = 'red';
-    ctx.strokeRect(snake[i].x, snake[i].y, box, box);
+  function animate(draw_to) {
+    ctx.beginPath();
+    ctx.arc(x, y, radiusStroke, start, draw_to, false);
+    ctx.stroke();
+    // Increment percent
+    curr++;
+    // Animate until end
+    if (curr < finish + 1) {
+      // Recursive repeat this function until the end is reached
+      requestAnimationFrame(function() {
+        animate(circumStroke * curr / 100 + start);
+      });
+    }
   }
-
-  ctx.drawImage(foodImg, food.x, food.y);
-
-  // old head position
-  let snakeX = snake[0].x;
-  let snakeY = snake[0].y;
-
-  // eats food
-  if (snakeX == food.x && snakeY == food.y) {
-    score++;
-    eatAud.play();
-    food = {
-      x: Math.floor(Math.random() * 17 + 1) * box,
-      y: Math.floor(Math.random() * 15 + 3) * box,
-    };
-  } else {
-    snake.pop();
-  }
-
-  let newHead = {
-    x: snakeX,
-    y: snakeY,
-  };
-
-
+  draw();
+  animate();
 }
-*/
+
+function newCircle() {
+  console.log('new circle call (Circles.legnth) ' + circles.length);
+  if (circles.length < 40) {
+    var circle = new Circle();
+    circles.push(circle);
+    console.log('new circle call if stat < 20');
+  }
+}
+newCircle();
