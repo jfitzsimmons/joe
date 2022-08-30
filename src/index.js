38aee04e95b1131fb78f7b73a0fd822568f5b119
jfitzsimmons <jfitzcode@gmail.com>
2022-08-30 16:40:20 -0500

    remove comments, shrunk bounds, new test animation

--- src/index.js
+++ src/index.js
@@ -19,43 +19,27 @@ function hslToHex(h, s, l) {
   }
   return `0x${f(0)}${f(8)}${f(4)}`
 }
-var Circle = function (bounds) {
+const Circle = function (bounds) {
   this.bounds = bounds
-  this.cx = Math.round(rndmRng(this.bounds.right, this.bounds.left))
-  this.cy = Math.round(rndmRng(this.bounds.bottom, this.bounds.top))
+  this.cx = Math.round(rndmRng(this.bounds.right - 5, this.bounds.left + 5))
+  this.cy = Math.round(rndmRng(this.bounds.bottom - 5, this.bounds.top + 5))
   this.start = Math.random() * Math.PI * 2
   this.speedX = Math.cos(this.start) / rndmRng(5, 1)
   this.speedY = Math.sin(this.start) / rndmRng(5, 1)
   this.radius = 0
   this.curr = 0
-  this.innerCrcmf = rndmRng(100, 25)
-  this.grooves = rndmRng(40, 10)
-  // this.outerRadius = Math.round(this.innerCrcmf * rndmRng(0.8, 0.6))
+  this.innerCrcmf = rndmRng(130, 25)
+  this.grooves = rndmRng(50, 10)
   this.color = rndmArrI(colors)
   this.light = rndmRng(60, 10)
   this.strokeColor = rndmArrI(strokeColors)
-
   this.graphics = new PIXI.Graphics()
   this.graphics.blendMode = PIXI.BLEND_MODES.XOR
-  //this.graphics.x = this.cx
-  //this.graphics.y = this.cy
 }
 
 Circle.prototype.update = function () {
-  //testjpf
-  //this conditional doesn't work becuase of
-  //his.innerCrcmf / this.groove sizes!!!
   if (this.radius < this.innerCrcmf && typeof this.radius !== 'undefined') {
     this.radius += Math.round(this.innerCrcmf / this.grooves)
-    /**  console.log(
-      `hsl 2 hex: ${hslToHex(
-        Math.round((this.color += 0.3)),
-        Math.round(100 - rndmRng(50, 0)),
-        Math.round((this.light += 0.1))
-      )}`
-    )*/
-    //this.graphics.blendMode.XOR
-
     this.graphics.beginFill(
       hslToHex(
         Math.round((this.color += 0.3)),
@@ -63,13 +47,6 @@ Circle.prototype.update = function () {
         Math.round((this.light += 0.1))
       )
     )
-    //this.graphics.beginFill(0x00ff00)
-
-    /**  this.graphics.beginFill = `hsl(${(this.color += 0.3)},${
-      100 - rndmRng(50, 0)
-    }%,${(this.light += 0.1)}%)`
-    this.ctx.fill()
-    */
 
     this.graphics.drawCircle(this.cx, this.cy, this.radius)
     return this.graphics
@@ -80,22 +57,15 @@ Circle.prototype.update = function () {
       .arc(
         this.cx,
         this.cy,
-        Math.round(this.innerCrcmf + rndmRng(35, 10)),
+        Math.round(this.innerCrcmf + rndmRng(45, 10)),
         this.start,
         (Math.PI * 2 * this.curr) / 100 + this.start,
         false
       )
-      .lineStyle(Math.round(rndmRng(20, 5)), `0x${this.strokeColor}`)
-    this.curr += 6.8
+      .lineStyle(Math.round(rndmRng(25, 5)), `0x${this.strokeColor}`)
+    this.curr += rndmRng(6.8, 3.4)
     if (this.curr == 100) this.graphics.cacheAsBitmap = true
   } else {
-    /**
-    console.log('else for movement')
-    console.log(`1||| this.cx: ${this.cx} | this.cy: ${this.cy}`)
-    console.dir(this.bounds)
-     */
-    //console.log(`this.graphics.x: ${this.graphics.x}`)
-    //console.log(`this.graphics.y: ${this.graphics.y}`)
     if (
       this.graphics.x + this.cx + this.innerCrcmf > this.bounds.right ||
       this.graphics.x + this.cx < this.bounds.left
@@ -110,78 +80,13 @@ Circle.prototype.update = function () {
 
     this.graphics.x -= this.speedX
     this.graphics.y -= this.speedY
-
-    // this.graphics.x = this.cx
-    //  this.graphics.y = this.cy
   }
-
-  /** 
-  c.ctx.beginPath()
-    c.ctx.arc(
-      c.outerRadius + 8,
-      c.outerRadius + 8,
-      Math.round(c.outerRadius),
-      c.start,
-      drawTo,
-      false
-    )
-    c.ctx.stroke()
-    c.curr += 1.7
-    if (c.curr < 101) {
-      c.request = requestAnimationFrame(function () {
-        addStroke(c, (Math.PI * 2 * c.curr) / 100 + c.start)
-      })
-    } else {
-      c.ani === 'rotate'
-        ? rotate(c)
-        : (c.request = requestAnimationFrame(() => bounce(c)))
-    }
-
-
-
-  //testjpf:  texture should be your canvas blob
-  //PIXI.Sprite.call(this, texture);
-  //    const pixiC = PIXI.Sprite.from(c.element)
-
-  if (
-    this.cx > this.bounds.right ||
-    this.cx < this.bounds.left
-  ) {
-    this.speedX *= -1
-  } else if (
-    this.cy < this.bounds.top ||
-    this.cy > this.bounds.bottom
-  ) {
-    this.speedY *= -1
-  }
-  this.cx += this.speedX
-  this.cy += this.speedY
-  */
 }
-
-/**
- * Don't use after this.
- * @method destroy
- */
-Circle.prototype.destroy = function () {
-  this.bounds = null
-  PIXI.Sprite.prototype.destroy.call(this)
-}
-
-/**
- * Application call for simulation
- * @class CircleStage
- * @param {String} domElementSelector Selector for the frame element
- */
-var CircleStage = function (domElementSelector) {
+const CircleStage = function (domElementSelector) {
   this.circles = []
 
   this.domElement = document.getElementById(domElementSelector)
 
-  /**
-   * Stage bounds
-   * @type {Object}
-   */
   this.bounds = {
     left: 0,
     top: 0,
@@ -189,21 +94,13 @@ var CircleStage = function (domElementSelector) {
     bottom: 0,
   }
 
-  /**
-   * Render for the stage
-   * @type {PIXI.CanvasRenderer|PIXI.WebGLRenderer}
-   */
   this.renderer = null
 
-  /**
-   * Container for the bunnies
-   * @type {PIXI.Container}
-   */
   this.stage = null
 }
 
 CircleStage.prototype.addCircles = function () {
-  var circle = new Circle(this.bounds)
+  const circle = new Circle(this.bounds)
 
   this.circles.push(circle)
 }
@@ -214,12 +111,13 @@ CircleStage.prototype.ready = function () {
     throw 'PIXI is required to run'
   }
 
-  var stage = document.getElementById('cvs0')
+  const stage = document.getElementById('cvs0')
 
   this.bounds.right = stage.offsetWidth
   this.bounds.bottom = stage.offsetHeight
 
-  var options = {
+  const options = {
+    backgroundAlpha: 0,
     view: stage,
     clearBeforeRender: true,
   }
@@ -240,19 +138,16 @@ CircleStage.prototype.ready = function () {
   this.stage.interactiveChildren = false
 
   // Handle window resizes
-  /** 
-  window.onresize = () => {
-    'resize orientationchange', this.resize.bind(this)
-  }
+  /**  window.onresize = () => {
+    this.resize.bind(this)
+  }*/
+
+  window.addEventListener('resize', this.resize.bind(this), true)
 
   this.resize()
-  */
+
   this.startUpdate()
-  /**
-   * testjpf
-   *
-   * with hardoced delay, se which circle appear, which don't and why they are different
-   */
+
   const circleAmount = Math.round(
     (this.bounds.right * this.bounds.bottom) / 44000
   )
@@ -263,54 +158,49 @@ CircleStage.prototype.ready = function () {
   }
 }
 
-// Window ready
 document.addEventListener('DOMContentLoaded', (e) => {
-  // frame
-  var app = new CircleStage('cvs0-container')
+  const app = new CircleStage('cvs0-container')
 
   app.ready()
 })
 
 CircleStage.prototype.startUpdate = function () {
-  // console.log(' FFF START UPDATE FFF ')
-  var _this = this
+  const _this = this
   requestAnimationFrame(function () {
     _this.update()
   })
 }
 
+/** 
+Circle.prototype.destroy = function () {
+  this.bounds = null
+  PIXI.Sprite.prototype.destroy.call(this)
+}
+*/
+
 /**
- * Resize the stage
- * @method resize
+ * testjpf
+ *
+ * debounce!!!
  */
 CircleStage.prototype.resize = function () {
-  var width = this.domElement.offsetWidth
-  var height = this.domElement.offsetHeight
+  console.log(`CircleStage.prototype.resize`)
+  const width = this.domElement.offsetWidth
+  const height = this.domElement.offsetHeight
   this.bounds.right = width
   this.bounds.bottom = height
   this.renderer.resize(width, height)
+  this.reset()
 }
 
-/**
- * Remove all bunnies
- * @method reset
- */
 CircleStage.prototype.reset = function () {
+  console.log(`CircleStage.prototype.reset`)
   this.stage.removeChildren()
-  for (var i = this.circles.length - 1; i >= 0; i--) {
-    var circle = this.circles[i]
-    circle.destroy()
-  }
   this.circles.length = 0
 }
 
-/**
- * Frame update function
- * @method update
- */
 CircleStage.prototype.update = function () {
   if (this.circles.length > 0) {
-    //  console.log(`this.circles.length: ${this.circles.length}`)
     for (let i = this.circles.length; i--; ) {
       let fart = this.circles[i].update()
       if (fart) this.stage.addChild(fart)
