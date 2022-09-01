64b1803d08caf2215f95ceb8e5769c1c6f62eedf
jfitzsimmons <jfitzcode@gmail.com>
2022-09-01 12:50:24 -0500

    Updates

---
+++ v2/js/init.js
@@ -0,0 +1,14 @@
+function smoothScroll(e) {
+  e.preventDefault();
+  let target = document.getElementById(this.getAttribute('data-href'));
+  let offset = target.offsetTop - window.innerHeight*.005;
+  window.scrollTo({
+    top: offset,
+    behavior: "smooth"
+  });
+};
+
+let links = document.querySelectorAll('[data-href]');
+links.forEach((l) => 
+  l.addEventListener("click", smoothScroll)
+);
